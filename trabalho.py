368544a2a3ed3e953b135349aa1bf5cc0d5f04d4
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-09-12 04:23:08 -0300

    Update to V1 code

--- trabalho.py
+++ trabalho.py
@@ -1,1 +1,16 @@
 l = []
+
+for _ in range (10):
+  l.append(int(input('digite seu numero:')))
+
+a = sorted(l)
+
+print (a,'organizado:')
+
+j = 0
+for i in l:
+    j = j + i
+
+media = j/len(l)
+print (media,'media:')
+    
