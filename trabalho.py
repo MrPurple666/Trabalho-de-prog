383bbd98db12fede7b23c629742e2a99030ee6c3
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-09-14 08:45:03 -0300

    init V2

--- trabalho.py
+++ trabalho.py
@@ -2,19 +2,15 @@
 #Codename = DragonMushroomGray
 #Creation date = 03/09/2021 03:35
 
-l = []
+i = input('digite seu numero:')
+l = i.split(',')
 
-for _ in range (10):
-  l.append(int(input('digite seu numero:')))
-
-a = sorted(l)
-
-print ('organizado:',a)
+#l = sorted(l)
+l = l.sort(reverse = True)
+print(l)
 
 j = 0
 for i in l:
-    j = j + i
-
-media = j/len(l)
-print (media,'media:')
-    
+ j = j + int(i)
+ 
+print("media=",j/len(l))
