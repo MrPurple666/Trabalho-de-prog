4bc5edc2ccba8938d4f24986aa87b9d375973406
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-09-15 14:02:20 -0300

    Update to Version 4 - Final

--- trabalho.py
+++ trabalho.py
@@ -1,18 +1,30 @@
-#Version 3 by MrPurple666
-#Codename = PokemonXDLugia
-#Creation date = 14/09/2021 23:05
+#Version 4 by MrPurple666
+#Codename = Final
+#Creation date = 15/09/2021 01:45
 
-i = input()
-l = i.split(',')
-lInt = []
+print("escreva 10 numeros:")
+x = input()
+y = x.split(' ')
+IInt = []
+pares = []
+impares = []
 
-for i in l:
- lInt.append(int(i))
+for x in y
+IInt.append(int(x))
 
-print(sorted(lInt,reverse=True))
+print ("Lista em ordem decrescente:")
+print(sorted(IInt,reverse=True))
 
-j = 0
-for i in l:
- j = j + int(i)
- 
-print("media=",j/len(l))
+z = 0
+for x in y:
+z = z + int(x)
+
+for f in IInt:
+if f % 2 == 0:
+pares.append(f)
+else:
+impares.append(f)
+
+print("impares:")
+print(impares)
+print("media=",z/len(y))
